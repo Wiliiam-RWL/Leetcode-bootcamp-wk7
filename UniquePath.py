1550110aa3cd056c7089635ef2fe63087ba23ce0
Weilin Ye <weilin@weilindndiannao.mynetworksettings.com>
2023-11-18 19:32:21 -0500

    acc

---
+++ UniquePath.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        res = [[1 for i in range(n)] for j in range(m)]
+        for i in range(1, m, 1):
+            for j in range(1, n, 1):
+                res[i][j] = res[i - 1][j] + res[i][j - 1]
+        return res[-1][-1]
